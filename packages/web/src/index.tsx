504915a923a9725b3e5d83b789299b8655a51b65
Frank Meza <frankmezapdx@gmail.com>
2018-10-11 14:47:48 -0700

    better plumbing through import/export

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,4 +1,4 @@
-import { store } from "@cashew/common"
+import { actions, store } from "@cashew/common"
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { Provider } from "react-redux"
@@ -6,9 +6,13 @@ import App from "./App"
 import "./index.css"
 import registerServiceWorker from "./registerServiceWorker"
 
+/* tslint:disable */
+console.log(Object.keys(actions))
+/* tslint:enable */
+
 ReactDOM.render(
     <Provider store={store}>
-        <App appState={store.getState()} />
+        <App actions={actions} appState={store.getState()} />
     </Provider>,
     document.getElementById("root") as HTMLElement,
 )
