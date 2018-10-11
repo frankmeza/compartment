ef404914c2c504d2f5387e91f4e6be1ec418b5d9
Frank Meza <frankmezapdx@gmail.com>
2018-10-11 13:40:24 -0700

    sanity check

--- packages/common/src/redux/sagas/index.ts
+++ packages/common/src/redux/sagas/index.ts
@@ -2,10 +2,12 @@ import { takeEvery } from "redux-saga/effects"
 
 /* tslint:disable */
 const reset = () => console.log("reset from the saga")
+const incr = () => console.log("incremented from the saga")
 /* tslint:enable */
 
-export default function* rootSaga(): Iterator<{}> {
-    yield [
-        takeEvery("RESET", reset),
-    ]
+/* tslint:disable */
+export default function* rootSaga(): Iterator<any> {
+    yield [takeEvery("RESET", reset)]
+    yield [takeEvery("INCREMENT", incr)]
 }
+/* tslint:enable */
