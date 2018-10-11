5ba89e8d2102e6594dff408b3ad8a4d673970a87
Frank Meza <frankmezapdx@gmail.com>
2018-10-11 13:40:12 -0700

    add types and comments

--- packages/common/src/redux/index.ts
+++ packages/common/src/redux/index.ts
@@ -4,7 +4,7 @@ import { createLogger } from "redux-logger"
 import { actions, counter } from "../redux/modules/counter"
 
 // begin imports without special characters
-import createSagaMiddleware from "redux-saga"
+import createSagaMiddleware, { SagaMiddleware } from "redux-saga"
 import rootSaga from "../redux/sagas"
 
 export interface AppState {
@@ -18,17 +18,38 @@ const appState: AppState = {
 const INITIAL_STATE: AppState = appState
 
 const appReducer: redux.Reducer = redux.combineReducers({ counter })
-const logger = createLogger({ stateTransformer: (state: AppState) => state })
-const sagaMiddleware = createSagaMiddleware()
-const middleware: ReadonlyArray<Middleware> = [sagaMiddleware, logger]
+const logger: Middleware = createLogger({
+    stateTransformer: (state: AppState) => state,
+})
+const sagaMiddleware: SagaMiddleware<{}> = createSagaMiddleware()
+const commonMiddleware: ReadonlyArray<Middleware> = [sagaMiddleware, logger]
+
+// we need a function that accepts more middleware and then returns a function that returns a store
+// then export that function
 
 const createStoreWithMiddleware = createStore(
     appReducer,
     INITIAL_STATE as redux.DeepPartial<{}>,
-    applyMiddleware(...middleware),
+    applyMiddleware(...commonMiddleware),
 )
 
 const store = createStoreWithMiddleware
+
+// fn that returns a wrapped store creator
+// storeCreatorWrapper :: clientMiddleware -> () -> sreateStoreWithMiddleWAre
+const storeCreatorWrapper = (clientMiddleWare: ReadonlyArray<Middleware>) => {
+    const combinedMiddleware = [...commonMiddleware, ...clientMiddleWare]
+    return () =>
+        createStore(
+            appReducer,
+            INITIAL_STATE as redux.DeepPartial<{}>,
+            applyMiddleware(...combinedMiddleware),
+        )
+}
+
+// fn that accepts middleware and returns a created store
+// storeCreator :: fn, Middleware -> redux.Store<any, redux.AnyAction> & { dispatch: {} }
+
 sagaMiddleware.run(rootSaga)
 
-export { actions, appState, store, INITIAL_STATE }
+export { actions, appState, store, storeCreatorWrapper, INITIAL_STATE }
